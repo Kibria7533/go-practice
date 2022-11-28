d35602b7cbfdf5c83a3cac5fa17687e592ed5b1e
Golam_kibria <kibria7533@A>
2022-11-28 16:52:16 +0600

    oparetor

---
+++ oprators.go
@@ -0,0 +1,14 @@
+package main
+import ("fmt")
+
+func main() {
+  var a = 15 / 25
+  a++;
+  fmt.Println(a)
+  b := 2;
+  if b !=1	{
+	fmt.Println(b)
+  }
+ 
+  
+}
--- slices.go
+++ slices.go
@@ -3,14 +3,24 @@ import ("fmt")
 
 func main() {
 
-	myslice := []int {1,2,3}
-	fmt.Println(cap(myslice))
+// 	myslice := []int {1,2,3}
+// 	fmt.Println(cap(myslice))
 
-	arr1 := [6]int{10, 11, 12, 13, 14,15}
-  myslice2 := arr1[2:4]
+// 	arr1 := [6]int{10, 11, 12, 13, 14,15}
+//   myslice2 := arr1[2:4]
 
-  fmt.Printf("myslice = %v\n", myslice)
-  fmt.Printf("length = %d\n", len(myslice2))
-  fmt.Printf("capacity = %d\n", cap(myslice))
+//   fmt.Printf("myslice = %v\n", myslice)
+//   fmt.Printf("length = %d\n", len(myslice2))
+//   fmt.Printf("capacity = %d\n", cap(myslice))
+
+myslice1 := []int{1, 2, 3, 4, 5, 6}
+  fmt.Printf("myslice1 = %v\n", myslice1)
+  fmt.Printf("length = %d\n", len(myslice1))
+  fmt.Printf("capacity = %d\n", cap(myslice1))
+
+  myslice1 = append(myslice1, 20, 21)
+  fmt.Printf("myslice1 = %v\n", myslice1)
+  fmt.Printf("length = %d\n", len(myslice1))
+  fmt.Printf("capacity = %d\n", len(myslice1))
 
 }
