5034943d9585532bee1c08e32736595214347659
Golam_kibria <kibria7533@A>
2022-11-28 16:19:34 +0600

    array length

--- array.go
+++ array.go
@@ -3,9 +3,16 @@ import ("fmt")
 
 func main() {
 
-  var arr3 = []string{"A"}
-  arr2 := [5]int{4,5,6,7,8}
+//   var arr3 = []string{"A"}
+//   arr2 := [5]int{4,5,6,7,8}
 
-  fmt.Println(arr3)
-  fmt.Println(arr2)
+//   fmt.Println(arr3[0])
+//   fmt.Println(arr2)
+
+
+  arr1 := [4]string{"Volvo", "BMW", "Ford", "Mazda"}
+  arr2 := [...]int{1,2,3,4,5,6}
+
+  fmt.Println(len(arr1))
+  fmt.Println(len(arr2))
 }
