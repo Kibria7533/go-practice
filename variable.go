da552e2ae75bcd66d9a9f734251427cd9dbc9b09
Golam_kibria <kibria7533@A>
2022-11-28 14:43:15 +0600

    variable decliaration

---
+++ variable.go
@@ -0,0 +1,10 @@
+package main;
+import "fmt"
+var a string = "kibria"
+// multiple cariable declairing
+var e, b, c, d int = 1, 3, 5, 7
+func main()  {
+	x := 4;
+    var y=8; 
+	fmt.Println(a,x,y,d);
+}
